84594e2d8360f2247b3b4909de3e45495278b06d
WilliamHashimoto <79861381+WilliamHashimoto@users.noreply.github.com>
2021-04-27 15:27:43 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,78 @@
+//Exo1
+// function add(x,y) {
+//     return console.log(x+y);
+// }
+
+// add(2,3);
+
+//Exo2
+
+// const soustraction = function (x,y) {
+//     return console.log(y - x);
+// }
+
+// soustraction(2,3);
+
+//Exo3
+// let multiplication = function (x,y) {
+//     return console.log(x*y);
+// }
+
+// multiplication(2,3);
+
+//Exo4
+// let division = (x,y) => {
+//     return console.log(x/y);
+// }
+
+// division(2,3);
+
+//Exo5
+// let modulo = (x,y) => {
+//     return console.log(x%y);
+// }
+
+// modulo(2,3);
+
+//Exo6
+// function racineCarree(x) {
+//     return console.log(Math.sqrt(x));
+// }
+
+// racineCarree(4);
+
+//Exo7
+// function exposant(x,y) {
+//     return console.log(Math.pow(x,y));
+// }
+
+// exposant(2,3);
+
+//Exo8
+// let capitalize = function (words) {
+//     return console.log(words.charAt(0).toUpperCase() + words.substr(1).toLowerCase());
+// }
+
+// capitalize("what's My Name");
+
+//Exo9
+// let calcul = (nbr1, operator, nbr2) => {
+//     switch (operator) {
+//         case "-":
+//             return nbr1 - nbr2;
+//             break;
+//         case "+":
+//             return nbr1 + nbr2;
+//             break;
+//         case "*":
+//             return nbr1 * nbr2;
+//             break;
+//         case "/":
+//             return nbr1 / nbr2;
+//             break;
+//         default:
+//             break;
+//     }
+// }
+
+// console.log(calcul(+prompt("entrez un nombre"), prompt("Entrez un operateur"), +prompt("Entrez un autre nombre")));
